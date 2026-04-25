600ff2705f4d8b8f50b663812aa8f7f772577caa
jaaonline <jaaonline8@gmail.com>
2026-04-25 14:57:47 -0400

    fix: allow all CORS origins

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,16 +1,16 @@
-import dotenv from 'dotenv'
-dotenv.config()
-
 import express from 'express'
 import cors from 'cors'
+import dotenv from 'dotenv'
 import { generateRoute } from './routes/generate'
 
-
+dotenv.config()
 
 const app = express()
 const PORT = process.env.PORT || 3001
 
-app.use(cors())
+app.use(cors({
+  origin: '*'
+}))
 app.use(express.json())
 
 app.use('/api', generateRoute)
